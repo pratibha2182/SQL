b393c54c0d0ddfa3ec75f564f229479859a760f9
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-17 09:49:28 +0000

    Delete_table_database

--- Delete_table_database.sql
+++ Delete_table_database.sql
@@ -3,3 +3,19 @@ DROP DATABASE startersql;
 
 Drop the table
 DROP table users;
+
+The DELETE statement removes rows from a table.
+
+
+To delete  one single row
+DELETE FROM users WHERE id = 3;
+
+To delete multiple row
+DELETE FROM users WHERE gender = 'Other';
+
+To delete all inseted but without table structure
+DELETE FROM users;
+
+
+To delete entire table
+DROP TABLE users;
